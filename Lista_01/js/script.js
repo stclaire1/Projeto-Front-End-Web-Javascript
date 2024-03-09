754ff7811e78617e7dbe6f9a8de8355b2143637c
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-03-09 12:29:19 -0300

    update script.js

--- Lista_01/js/script.js
+++ Lista_01/js/script.js
@@ -31,83 +31,54 @@ maxAge = vet => {
     return maxAge;
 }
 
-// oddAge = vet => {
-//     var vetOdd = [];
-//     for(var i = 0; i<vet.length; i++){
-//         if(vet[i] % 2 != 0){
-//             vetOdd.push(vet[i]);
-//         }
-//     }
-    
-//     return vetOdd;
-// }
-
 oddAge = vet => {
-    var vetOdd = vet.filter((index) => index % 2 != 0);
-
+    var vetOdd = [];
+    for(var i = 0; i<vet.length; i++){
+        if(vet[i] % 2 != 0){
+            vetOdd.push(vet[i]);
+        }
+    }
+    
     return vetOdd;
 }
 
-// legalAge = vet => {
-//     var verification = true;
-//     for(var i = 0; i<vet.length; i++){
-//         if(vet[i] <= 18) verification = false;
-//     }
-    
-//     return verification;
-// }
 
 legalAge = vet => {
-    var verification = vet.every((index) => index >= 18);
-
+    var verification = true;
+    for(var i = 0; i<vet.length; i++){
+        if(vet[i] <= 18) verification = false;
+    }
+    
     return verification;
 }
 
-// userNumber = (vet, userNum) => {
-//     var verification = true;
-//     for(var i = 0; i<vet.length; i++){
-//         if(vet[i] < userNum) verification = false;
-//     }
-
-//     return verification ? "Todas as idades são maiores ou iguais ao valor informado pelo usuário" : "Pelo menos uma das idades é menor que o valor informado pelo usuário"
-// }
 
 userNumber = (vet, userNum) => {
-    var verification = vet.every((index) => index >= userNum);
+    var verification = true;
+    for(var i = 0; i<vet.length; i++){
+        if(vet[i] < userNum) verification = false;
+    }
 
-    return verification ? "Todas as idades são maiores ou iguais ao valor informado pelo usuário" : "Pelo menos uma das idades é menor que o valor informado pelo usuário";
+    return verification ? "Todas as idades são maiores ou iguais ao valor informado pelo usuário" : "Pelo menos uma das idades é menor que o valor informado pelo usuário"
 }
 
-// displayAges = (vet, userNum) => {
-//     var vetMaxAge = [];
-//     for(var i = 0; i<vet.length; i++){
-//         if(vet[i] >= userNum) vetMaxAge.push(vet[i]);
-//     }
-
-//     return vetMaxAge;
-// }
-
 displayAges = (vet, userNum) => {
-    var vetMaxAge = vet.filter((index) => index >= userNum);
+    var vetMaxAge = [];
+    for(var i = 0; i<vet.length; i++){
+        if(vet[i] >= userNum) vetMaxAge.push(vet[i]);
+    }
 
     return vetMaxAge;
 }
 
-// avgMaxAge = maxAges => {
-//     var sum = 0;
-//     for(var i = 0; i<maxAges.length; i++){
-//         sum+=maxAges[i];
-//     }
 
-//     var avg = sum/maxAges.length;
-//     avg = avg.toFixed(2);
+avgMaxAge = maxAges => {
+    var sum = 0;
+    for(var i = 0; i<maxAges.length; i++){
+        sum+=maxAges[i];
+    }
 
-//     return avg;
-// }
-
-avgMaxAge = displayAges => {
-    var sum = displayAges.reduce((acc, index) => acc + index);
-    var avg = sum/displayAges.length;
+    var avg = sum/maxAges.length;
     avg = avg.toFixed(2);
 
     return avg;
