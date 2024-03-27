5f2ab24d3285573065aa570a1e1f9c5d99599a80
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-03-27 00:25:15 -0300

    update script

--- Lista_04/js/script.js
+++ Lista_04/js/script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", () => {
         method: 'GET',
         headers: {
           accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODVmMmMwYzJiMjkxZTM3ZTQxYzY1YTZjZTBlYmQ3NCIsInN1YiI6IjY0MTIyNWQ3ZWRlMWIwMjhhMmI4NWVkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VKHFk792x8CFl9Cdx6xDwYP4Ia5Dt5GiJxENktQHGhQ'
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTJjOTg5MGU5M2YwZmRhYTg5ODQ3OTA5NGI3MGJmMyIsInN1YiI6IjY2MDMwZTIwNDU5YWQ2MDE4N2ZjNWJmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mWhXCNN1sVEQH4xijfXs4GjiYOScAR8_kpTt--p5gME'
         }
       };
       
@@ -18,7 +18,20 @@ window.addEventListener("load", () => {
 
 function displayTitles(movies) {
     movies.forEach(movie => {
-        var boxMovie = document.createElement("div");
+        var movieContainer = document.createElement("div");
+        movieContainer.classList.add("movieInfo");
+
+        var infoDiv = document.createElement("div");
+        infoDiv.classList.add("infoDiv");
+
+        var groupInfoDiv = document.createElement("div");
+        groupInfoDiv.classList.add("groupInfoDiv");
+
+        var titleGroupDiv = document.createElement("div");
+        titleGroupDiv.classList.add("titleGroupDiv");
+
+        var rating = document.createElement("div");
+        rating.classList.add("rating");
 
         // Criando o parágrafo
         var movieTitle = document.createElement("p");
@@ -27,11 +40,62 @@ function displayTitles(movies) {
         // Criando a imagem 
         var moviePoster = document.createElement("img");
         var url = "https://image.tmdb.org/t/p/w500";
-        // moviePoster.setAttribute("src",`${url}${filme.backdrop_path}`);
         moviePoster.setAttribute("src",`${url}${movie.poster_path}`);
 
-        boxFilme.appendChild(movieTitle);
-        boxFilme.appendChild(moviePoster);
-        document.getElementById("boxMovie").appendChild(boxMovie);
+        //Criando o ano de lançamento
+        var movieReleaseYear = document.createElement("p");
+        movieReleaseYear.textContent = movie.release_date.substring(0, 4);
+
+        titleGroupDiv.appendChild(movieTitle);
+        groupInfoDiv.appendChild(movieReleaseYear);
+        
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTJjOTg5MGU5M2YwZmRhYTg5ODQ3OTA5NGI3MGJmMyIsInN1YiI6IjY2MDMwZTIwNDU5YWQ2MDE4N2ZjNWJmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mWhXCNN1sVEQH4xijfXs4GjiYOScAR8_kpTt--p5gME'
+            }
+          };
+          
+          var language = "pt-br";
+
+        //Criando a duração, avaliação e sinopse
+        fetch(`https://api.themoviedb.org/3/movie/${movie.id}?language=${language}`, options)
+            .then(response => response.json())
+            .then(movieDetails => {
+                var movieRunTime = document.createElement("p");
+                movieRunTime.textContent = movieDetails.runtime + " minutos";
+
+                var movieOverview = document.createElement("p");
+                movieOverview.textContent = movieDetails.overview.substring(0, 250) + "...";
+
+                var movieRating = document.createElement("p");
+                movieRating.innerHTML =  movie.vote_average.toFixed(1);
+
+                var starIcon = document.createElement("span");
+                starIcon.classList.add("fa", "fa-star");
+
+                groupInfoDiv.appendChild(movieRunTime);
+                rating.appendChild(starIcon);
+                rating.appendChild(movieRating);
+                groupInfoDiv.appendChild(rating);
+                titleGroupDiv.appendChild(groupInfoDiv)
+                infoDiv.appendChild(titleGroupDiv);
+                infoDiv.appendChild(movieOverview);
+
+                movieContainer.addEventListener("mouseover", () => {
+                  movieOverview.textContent = movieDetails.overview;
+              });
+
+              movieContainer.addEventListener("mouseout", () => {
+                  movieOverview.textContent = movieDetails.overview.substring(0, 200) + "...";
+              });
+          });
+        
+        movieContainer.appendChild(moviePoster);
+        movieContainer.appendChild(infoDiv);
+
+        document.getElementById("boxMovie").appendChild(movieContainer);
     })
 }
+
